44b53efedc64f234d693a7d3c3069bf94a715ce4
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2019-11-25 23:31:30 +0530

    Create server.js

---
+++ mongo-conn/server.js
@@ -0,0 +1,9 @@
+var http = require('http')
+var {app} = require("./app.js")
+var server = http.createServer(app)
+var port = process.env.PORT || 3000
+
+server.listen(port,()=>
+{
+    console.log('Server running at http://localhost:'+port)
+})
